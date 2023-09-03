e448077f710c3a3c73f44dcfae43cb069eb82264
Artur Długosz <artur.dlugosz@microsoft.wsei.edu.pl>
2023-09-03 23:11:39 +0200

    Dodaj pliki projektów.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ aspl5/Task.cs
@@ -0,0 +1,20 @@
+﻿
+namespace aspl5
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+        public Task(string title)
+        {
+            Title = title;
+            IsCompleted = false;
+        }
+
+    }
+
+
+}
